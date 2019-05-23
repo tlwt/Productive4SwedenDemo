81913446a721565e507a1d67e4713ab0f7473b9b
Noah Winneberger <winneberger@MacBook-Pro.fritz.box>
2019-05-23 22:29:00 +0200

    Updated sockets

--- kukaDemo/sockets/serverSocket.py
+++ kukaDemo/sockets/serverSocket.py
@@ -13,3 +13,15 @@ socketClient = zerorpc.Client()
 socketClient.connect('tcp://89.144.27.100:4242')
 print('Successful Connection to the ZeroRPC Server...')
 
+# Create the tcp socket and send the Blockchain information to the client
+# On successful connection execute the advanceContract function and send info back
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serverSocket.bind(('89.144.27.100', 5252))
+serverSocket.listen(1)
+conn, addr = serverSocket.accept()
+while 1:
+    data = conn.recv(1024)
+    if not data:
+        break
+    conn.sendall(data)
+conn.close()
