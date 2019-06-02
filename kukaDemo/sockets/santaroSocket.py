333e219cccfef1bc6d387cc4e2a029e9510ec8f8
Noah Winneberger <winneberger@MacBook-Pro.fritz.box>
2019-06-02 12:38:52 +0200

    Updated the final sockets

--- kukaDemo/sockets/santaroSocket.py
+++ kukaDemo/sockets/santaroSocket.py
@@ -9,8 +9,42 @@ print("###########################################################")
 
 # Creating the client socket to connect to the external server and receive the relevant Blockchain information
 socketClient = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-socketClient.connect(('89.144.27.100', 5252))
-socketClient.sendall('Hello, world')
+socketClient.connect(('localhost', 5252))
 data = socketClient.recv(1024)
 socketClient.close()
 print 'Received', repr(data)
+
+# creating the unix domain socket and write out the trigger for the UI
+def waitingForConnection(sock):
+    while True:
+        print >> sys.stderr, 'waiting for a connection'
+        connection, client_address = sock.accept()
+        try:
+            print >> sys.stderr, 'connection from', client_address
+            while True:
+                connection.send('blockchainTrigger')
+                print c.advanceContract(state)
+            else:
+                print >> sys.stderr, 'no more confirmation possible', client_address
+                break
+        finally:
+            print('All information was sent, waiting for new connection...')
+            newConnection()
+
+
+def newConnection():
+    print('The current state of the Smart Contract: ', _state)
+    server_address = '/tmp/blockchainSocket.sock'
+    try:
+        os.unlink(server_address)
+    except OSError:
+        if os.path.exists(server_address):
+            raise
+    sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+    print >> sys.stderr, 'starting up on %s' % server_address
+    sock.bind(server_address)
+    sock.listen(1)
+    waitingForConnection(sock, _state)
+
+
+newConnection()
