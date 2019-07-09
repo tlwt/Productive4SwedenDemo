5d872e83e94574a353547a331cbec3f9226be5c1
Noah Winneberger <winneberger@MacBook-Pro.local>
2019-07-09 16:26:50 +0200

    Updated server scripts

--- dobotDemo/sockets/dobotCommandServer.py
+++ dobotDemo/sockets/dobotCommandServer.py
@@ -13,18 +13,9 @@ print("###########################################################")
 # Connection to central logic and the transfer of the relevant information
 # Will have to change the socket name and the flow of information 
 # serversocket will have to be the dobot otherwise I will have to many separate instances
-class dobotContract(object):
-    def newConnection():
-    # Creating the client socket to connect to the external server and receive the relevant Blockchain information
-    socketClient = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    socketClient.connect(('192.168.178.108', 50001))
+socketClient = zerorpc.Client()
+socketClient.connect('tcp://89.144.27.100:4242')
+print('Successful Connection to the ZeroRPC Server...')
+print socketClient.newConnection()
 
-    # Sending the command byte message
-        MESSAGE = "\x01\x00\x0f\x6d\x61\x67\x6e\x65\x74\x5f\x74\x65\x73\x74\x2e\x62\x69\x6e"
-        socketClient.sendall(MESSAGE)
-    return("Successful Blockchain interaction...")
-
-s = zerorpc.Server(dobotContract())
-s.bind("tcp://0.0.0.0:4242")
-s.run()
 
---
+++ dobotDemo/sockets/serverSocket.py
@@ -0,0 +1,44 @@
+import zerorpc
+import os
+import sys
+import socket
+print("###########################################################")
+print("#														 #")
+print("#	Socket script for Blockchain to KUKA interaction	 #")
+print("#														 #")
+print("###########################################################")
+
+# Creation of ZeroRPC Client for Blockchain Interaction 
+socketClient = zerorpc.Client()
+socketClient.connect('tcp://89.144.27.100:4242')
+print('Successful Connection to the ZeroRPC Server...')
+
+# Creation of ZeroRPC Server for Dobot Interaction
+class dobotContract(object):
+    def newConnection():
+    # Creating the client socket to connect to the external server and receive the relevant Blockchain information
+    socketClient = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socketClient.connect(('192.168.178.108', 50001))
+
+    # Sending the command byte message
+        MESSAGE = "\x01\x00\x0f\x6d\x61\x67\x6e\x65\x74\x5f\x74\x65\x73\x74\x2e\x62\x69\x6e"
+        socketClient.sendall(MESSAGE)
+    return("Successful Blockchain interaction...")
+
+s = zerorpc.Server(dobotContract())
+s.bind("tcp://0.0.0.0:3232")
+s.run()
+
+# Create the tcp socket and send the Blockchain information to the client
+# On successful connection execute the advanceContract function and send info back
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serverSocket.bind(('89.144.27.100', 5252))
+serverSocket.listen(1)
+conn, addr = serverSocket.accept()
+while 1:
+    data = conn.recv(1024)
+    print socketClient.advanceContract()
+    if not data:
+        break
+    conn.sendall(data)
+conn.close()
