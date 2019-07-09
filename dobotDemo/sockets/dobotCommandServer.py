a73df42f1fc44fb83c7ea7f2690515c1393cc969
Noah Winneberger <winneberger@MacBook-Pro.local>
2019-07-09 16:32:34 +0200

    Updated files

--- dobotDemo/sockets/dobotCommandServer.py
+++ dobotDemo/sockets/dobotCommandServer.py
@@ -14,7 +14,7 @@ print("###########################################################")
 # Will have to change the socket name and the flow of information 
 # serversocket will have to be the dobot otherwise I will have to many separate instances
 socketClient = zerorpc.Client()
-socketClient.connect('tcp://89.144.27.100:4242')
+socketClient.connect('tcp://89.144.27.100:3232')
 print('Successful Connection to the ZeroRPC Server...')
 print socketClient.newConnection()
 
--- dobotDemo/sockets/serverSocket.py
+++ dobotDemo/sockets/serverSocket.py
@@ -14,16 +14,16 @@ socketClient.connect('tcp://89.144.27.100:4242')
 print('Successful Connection to the ZeroRPC Server...')
 
 # Creation of ZeroRPC Server for Dobot Interaction
-class dobotContract(object):
+class dobotContract():
     def newConnection():
     # Creating the client socket to connect to the external server and receive the relevant Blockchain information
-    socketClient = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    socketClient.connect(('192.168.178.108', 50001))
-
+        socketClient = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        socketClient.connect(('192.168.178.108', 50001))
+        print('Sucessful connection to the dobot Server')
     # Sending the command byte message
         MESSAGE = "\x01\x00\x0f\x6d\x61\x67\x6e\x65\x74\x5f\x74\x65\x73\x74\x2e\x62\x69\x6e"
         socketClient.sendall(MESSAGE)
-    return("Successful Blockchain interaction...")
+        return("Successful Blockchain interaction...")
 
 s = zerorpc.Server(dobotContract())
 s.bind("tcp://0.0.0.0:3232")
